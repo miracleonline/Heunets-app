132dd99aa7da279e4b6836d2556ab8ae3f2a3b4d
Miracle Nwabuisi <thebluelogic@gmail.com>
2025-10-06 17:28:22 +0100

    fix: updated files to resolve frontend deploy errors

--- frontend/src/pages/TaskList.tsx
+++ frontend/src/pages/TaskList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getTasks } from '../api/tasks';
 import { Link } from 'react-router-dom';
 
